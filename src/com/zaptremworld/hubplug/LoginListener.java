3183007417c7d549c126f8b480d708cb157c41a1
Ryan Tremblay <zaptrem@me.com>
2013-05-06 12:07:08 -0400

    Started work on config for mysql

--- src/com/zaptremworld/hubplug/LoginListener.java
+++ src/com/zaptremworld/hubplug/LoginListener.java
@@ -1,7 +1,7 @@
 package com.zaptremworld.hubplug;
 
 import java.util.logging.Logger;
-
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -25,28 +25,10 @@ Connection conn;
         log= logger;
         
     
-	
-	
-	try {                                // to do: add this to a config file
-	    conn =
-	       DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-	                                   "user=monty&password=greatsqldb");
-
-	    // Do something with the Connection
-
-	   
-	} catch (SQLException ex) {
-	    // handle any errors
-		conn = null;
-	    System.out.println("SQLException: " + ex.getMessage());
-	    System.out.println("SQLState: " + ex.getSQLState());
-	    System.out.println("VendorError: " + ex.getErrorCode());
-	}
-	}
     @EventHandler
-    public void onLogin(PlayerLoginEvent event) {
+    public void onLogin PlayerLoginEvent event {
     	
-    	String user = event.getPlayer().getDisplayName();
+        String user = event.getPlayer().getDisplayName();
     	double money;
 		try {
 			money = (Economy.getMoney(user));
@@ -58,48 +40,7 @@ Connection conn;
     	log.info("User "+user+" has "+money+" zapbucks. ZING!");
     
     }
- public void writesql(){
-	 Statement stmt = null;
-	 ResultSet rs = null;
-
-	 try {
-		 // name is username and data is a table in the database
-	     stmt = conn.createStatement();
-	     rs = stmt.executeQuery("SELECT name FROM data");
-
-	    
-
-	     // Now do something with the ResultSet ....
-	 }
-	 catch (SQLException ex){
-	     // handle any errors
-	     System.out.println("SQLException: " + ex.getMessage());
-	     System.out.println("SQLState: " + ex.getSQLState());
-	     System.out.println("VendorError: " + ex.getErrorCode());
-	 }
-	 finally {
-	     // it is a good idea to release
-	     // resources in a finally{} block
-	     // in reverse-order of their creation
-	     // if they are no-longer needed
-
-	     if (rs != null) {
-	         try {
-	             rs.close();
-	         } catch (SQLException sqlEx) { } // ignore
-
-	         rs = null;
-	     }
-
-	     if (stmt != null) {
-	         try {
-	             stmt.close();
-	         } catch (SQLException sqlEx) { } // ignore
-
-	         stmt = null;
-	     }
-	 }
-	 
+ 
 	 
 	 
  }
--- src/com/zaptremworld/hubplug/LoginPlugin.java
+++ src/com/zaptremworld/hubplug/LoginPlugin.java
@@ -1,9 +1,15 @@
 package com.zaptremworld.hubplug;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+
+
 
 public class LoginPlugin extends JavaPlugin {
     public void onEnable() {
+    		// Save a copy of the default config.yml if one is not there
+            this.saveDefaultConfig();
         new LoginListener(this, getLogger());
+        
     }
 }
